561e5dbe3e51380fd3deb8a8363b1c4ef9eb63aa
scarf <greenscarf005@gmail.com>
2021-12-30 18:28:39 +0900

    feat: :sparkles: getting from pid, part 1

--- SafebooruDownloader/__init__.py
+++ SafebooruDownloader/__init__.py
@@ -1,12 +1,18 @@
+from __future__ import annotations
+
 from asyncio import run
 from dataclasses import dataclass, field
+from time import time
 from typing import Optional
 
 from aiohttp.client import ClientSession
 from aiopath import AsyncPath
 from bs4 import BeautifulSoup as Soup
+from bs4.element import NavigableString, Tag
+from yarl import URL
 
-from config import Config
+from config import Config, Params
+
 
 class Engine:
     def __init__(self, session: ClientSession, config: Config) -> None:
@@ -21,8 +27,29 @@ class Engine:
             if not await path.exists():
                 await path.write_bytes(await res.read())
 
-    async def getlinks(self):
+    async def initlinks(self):
+        url, params = self.config.baseurl, self.config.params
+        async with self.session.get(url, params=params) as res:
+            soup = Soup(await res.text(), "html.parser")
+            # anchors = soup.find_all("a", href=True)
+            # imgs = [a["href"] for a in anchors if a.find("img")]
+
+        alt = soup.find("a", alt="last page")
+        if not alt:
+            return
+        last = URL(alt["href"]).query["pid"]  # type: ignore
+        pids = [i * 40 for i in range(1, int(last) // 40)]
+
+        print(pids)
+        # print(imgs)
+        # for a in :
+        #     print(a["href"])
+
+        # return [a["href"] for a in links]
+
+    async def fetch_all(self):
         ...
+        # async with self.session(loop=loop)
 
     #     print(img.headers.get("content-type"))
     #     print(img.status)
@@ -39,14 +66,12 @@ test_url = "https://safebooru.org//samples/3598/sample_a406fd8bba13072c4c3db4ceb
 
 
 async def main():
-    config = Config("touhou scenery")
-    print(config)
-    exit()
+    config = Config("touhou 1girl scenery")
 
     async with ClientSession() as session:
         downloader = Engine(session, config)
-        await downloader.save(test_url)  # "https://www.google.com"
-        # await downloader.save()
+        print(await downloader.initlinks())
+        # await downloader.save(test_url)  # "https://www.google.com"
 
 
 run(main())
--- SafebooruDownloader/config.py
+++ SafebooruDownloader/config.py
@@ -1,17 +1,20 @@
 from dataclasses import dataclass, field
+
 from aiopath import AsyncPath
+from yarl import URL
+
+Params = dict[str, str]
 
 @dataclass
 class Config:
     _raw_tags: str = field(repr=False)
     path: AsyncPath = field(default=AsyncPath("img"))
     tags: list[str] = field(init=False)
-    baseurl: str = field(init=False, repr=False)
-    params: dict[str, str] = field(init=False, repr=False)
+    baseurl: URL = field(init=False, repr=False)
+    params: Params = field(init=False, repr=False)
 
     def __post_init__(self):
         self.tags = self._raw_tags.split(" ")
         self.path /= "-".join(self.tags)
-        self.baseurl = "http://safebooru.org/index.php?page=post"
-        self.params = {"s": "list", "tags": "+".join(self.tags)}
-
+        self.baseurl = URL("http://safebooru.org/index.php?page=post")
+        self.params = {"s": "list", "tags": self._raw_tags}
